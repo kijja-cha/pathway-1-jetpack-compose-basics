749210531540f828289f263d1fd7f08ab38fc40b
Kijja Charuchinda <kijja.clt@gmail.com>
2022-04-13 21:47:02 +0700

    Animating your list

--- app/src/main/java/com/kijja/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/kijja/basicscodelab/MainActivity.kt
@@ -3,6 +3,9 @@ package com.kijja.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -31,7 +34,11 @@ fun MyApp() {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        OnboardingScreen(
+            onContinueClicked = {
+                shouldShowOnboarding = false
+            }
+        )
     } else {
         Greetings()
     }
@@ -49,11 +56,17 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 @Composable
 private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) {
-        48.dp
-    } else {
-        0.dp
-    }
+    val extraPadding by animateDpAsState(
+        if (expanded.value) {
+            48.dp
+        } else {
+            0.dp
+        },
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -62,7 +75,7 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
